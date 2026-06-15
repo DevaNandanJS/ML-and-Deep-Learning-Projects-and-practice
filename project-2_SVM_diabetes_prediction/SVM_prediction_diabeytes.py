96421aeb7419de4d41af43fbb58909cf0e7b9578
DevaNandanJS <devanandan.connect@gmail.com>
2026-06-15 18:18:53 +0530

    progress save svm diabetes project, EDA done

--- project-2_SVM_diabetes_prediction/SVM_prediction_diabeytes.py
+++ project-2_SVM_diabetes_prediction/SVM_prediction_diabeytes.py
@@ -1,11 +1,21 @@
 # importing libraries
 import numpy as np
-import panas as pd
-import matplotlip.pyplot as plt
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import standardscaler
+from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
 #loading the dataset
-diabetes_dataset= pd.read_csv("datasets/diabetes.csv")
+diabetes_dataset= pd.read_csv(r"C:\ML-Practice-and-Projects\datasets\diabetes.csv")
+
+#printing first 5 rows
+print(diabetes_dataset.head())
+
+# number of rows and columns
+print(diabetes_dataset.shape)
+
+# understanding the data, statistical measures
+print(diabetes_dataset.describe())
+
+print(f"Value counts of Outcome: {diabetes_dataset['Outcome'].value_counts()}")
